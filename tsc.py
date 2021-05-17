188f952e8b2a684cd591c2ab2885219ddb65a389
Candisesys <johnstove482@gmail.com>
2021-05-17 14:50:50 -0400

    Initial commit

---
+++ tsc.py
@@ -0,0 +1,7 @@
+import random
+print("o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o")
+print("o               USELESS SQUARE ROOT CALCULATOR                    o")
+print("o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o")
+
+num = int(input("What is the number you want to find the square root off?"))
+print(num ** 2 ** random.randint(1,10))
